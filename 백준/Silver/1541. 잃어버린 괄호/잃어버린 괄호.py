48d35b3103a26df5724835803c6b2eb24ecb6218
chaeyeon0101 <87770634+chaeyeon0101@users.noreply.github.com>
2023-02-25 05:36:08 +0900

    [Silver II] Title: 잃어버린 괄호, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.py
+++ 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.py
@@ -5,29 +5,28 @@ answer = 0
 total = 0
 
 for i in range(len(n)):
-    if n[i] == "-":
-        total += int(n[p : i])
-        answer += total
-        total = 0
-        p = i + 1
-        break
-    elif n[i] == "+":
-        answer += int(n[p : i])
-        p = i + 1
     if i == len(n) - 1:
-        answer += int(n[p :])
+        total += int(n[p : ])
+        answer += total
         print(answer)
         exit()
+    if n[i].isdigit():
+        continue
+    total += int(n[p : i])
+    p = i + 1
+    if n[i] == "-":
+        answer += total
+        total = 0
+        break
 
 for i in range(p, len(n)):
+    if n[i].isdigit():
+        continue
+    total += int(n[p : i])
+    p = i + 1
     if n[i] == "-":
-        total += int(n[p : i])
         answer -= total
         total = 0
-        p = i + 1
-    elif n[i] == "+":
-        total += int(n[p : i])
-        p = i + 1
 
 total += int(n[p :])
 answer -= total
