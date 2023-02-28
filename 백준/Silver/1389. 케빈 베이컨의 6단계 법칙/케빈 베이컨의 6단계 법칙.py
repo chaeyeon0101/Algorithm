f4cfa898494976921c82bbb3b75bbd5a004d91ef
chaeyeon0101 <87770634+chaeyeon0101@users.noreply.github.com>
2023-02-28 13:45:48 +0900

    [Silver I] Title: 케빈 베이컨의 6단계 법칙, Time: 64 ms, Memory: 34128 KB -BaekjoonHub

--- 백준/Silver/1389. 케빈 베이컨의 6단계 법칙/케빈 베이컨의 6단계 법칙.py
+++ 백준/Silver/1389. 케빈 베이컨의 6단계 법칙/케빈 베이컨의 6단계 법칙.py
@@ -1,23 +1,31 @@
 import sys
 input = sys.stdin.readline
-INF = int(1e9)
+from collections import deque
+
+def bfs(v):
+    queue = deque([v])
+    visited[v] = 1
+    
+    while(queue):
+        target = queue.popleft()
+        
+        for i in graph[target]:
+            if not visited[i]:
+                visited[i] = visited[target] + 1
+                queue.append(i)
 
 n, m = map(int, input().split())
-graph = [[INF] * (n + 1) for _ in range(n + 1)]
-for i in range(1, n + 1):
-    graph[i][i] = 0
-
-for _ in range(m):
+graph = [[] for _ in range(n + 1)]
+for i in range(m):
     a, b = map(int, input().split())
-    graph[a][b] = 1
-    graph[b][a] = 1
+    graph[a].append(b)
+    graph[b].append(a)
 
-for k in range(1, n + 1):
-    for i in range(1, n + 1):
-        for j in range(1, n + 1):
-            graph[i][j] = min(graph[i][j], graph[i][k] + graph[k][j])
+answer = []
 
-answer = [INF] * (n + 1)
 for i in range(1, n + 1):
-    answer[i] = sum(graph[i][1 :])
-print(answer.index(min(answer)))
+    visited = [0] * (n + 1)
+    bfs(i)
+    answer.append(sum(visited))
+    
+print(answer.index(min(answer)) + 1)
